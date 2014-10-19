5939bdf0bf5e8a57d93276f80b81fa3077ac7be9
Stephen Sugden <glurgle@gmail.com>
2014-10-19 15:20:10 +0200

    Use `app.routes` in example app.

--- examples/per-request-values/server.js
+++ examples/per-request-values/server.js
@@ -5,10 +5,7 @@ app.value('translations', require('./translations'));
 app.request.value('language', require('./request/language'));
 app.request.value('messages', require('./request/messages'));
 
-var routes = require('./routes');
-for (var pattern in routes) {
-  app.route(pattern, routes[pattern]);
-}
+app.routes(require('./routes'));
 
 if (!module.parent) {
   app.listen(8484).on('listening', function () {
