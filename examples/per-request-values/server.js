182d0467427144acd47d7ef94f2c0273c3e3007a
Stephen Sugden <glurgle@gmail.com>
2014-10-19 15:21:25 +0200

    Fix a whopsie

--- examples/per-request-values/server.js
+++ examples/per-request-values/server.js
@@ -1,11 +1,13 @@
 'use strict';
 var app = require('../../')();
+var routes = require('./routes');
+
 app.value('translations', require('./translations'));
 
 app.request.value('language', require('./request/language'));
 app.request.value('messages', require('./request/messages'));
 
-app.routes(require('./routes'));
+app.routes(routes);
 
 if (!module.parent) {
   app.listen(8484).on('listening', function () {
